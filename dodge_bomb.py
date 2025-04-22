dfdc089a682b86e008f5a0104b83c85c99c3af12
c0c2400407 <c0c2400407@edu.teu.ac.jp>
2025-04-22 14:24:16 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,19 +1,36 @@
 import os
+import random
 import sys
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+DELTA = {
+    pg.K_UP(0, -5),
+    pg.K_DOWN(0, +5),
+    pg.K_LEFT(-5, 0),
+    pg.K_RIGHT(+5, 0),
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    #koukatonsyokika
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    #bakudannsyokika
+    bb_img = pg.surface((20, 20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_rct = bb_img.get_rect
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    bb_img.set_colorkey((0, 0, 0))
+    vx, vy = +5
+
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -23,17 +40,17 @@ def main():
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        sum_mv = [0, 0]#合計移動量の蓄積        
+        for key, mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]#左右方向
+                sum_mv[1] += mv[1]#上下方向
+
         kk_rct.move_ip(sum_mv)
-        screen.blit(kk_img, kk_rct)
+        screen.blit(kk_img, kk_rct)#bakudannnoidou
+        bb_rct.move_ip(vx, vy)#bakudannbyouga
+        screen.blit(bb_img,bb_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
